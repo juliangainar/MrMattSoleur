66a08b9cee9d3373addbe227628f7477f17957fb
Iulian Gainar <66164914+juliangainar@users.noreply.github.com>
2023-02-04 19:22:27 +0000

    Added : - Début de code de la classe ListeTableau

---
+++ fr/README.md
@@ -0,0 +1,2 @@
+Question : quel est le type du tableau à utiliser ?
+Vu que la classe est générique (ListeTableau<E>), le type de tableau serait le type générique E : private E[] tableau;
--- fr/rodez3il/a2022/mrmatt/solveur/structures/ListeTableau.java
+++ fr/rodez3il/a2022/mrmatt/solveur/structures/ListeTableau.java
@@ -3,15 +3,36 @@ package fr.rodez3il.a2022.mrmatt.solveur.structures;
 import java.util.Arrays;
 
 public class ListeTableau<E> implements Liste<E> {
+
   // Attributs
+  private E[] tableau;
 
   // Constructeurs
-
+  public ListeTableau(int tailleInitiale) {
+    this.tableau = (E[]) new Object[tailleInitiale];
+  }
   // Accesseurs
 
   // Méthodes
 
-  void ajouter(T element){
-    
+
+  @Override
+  public boolean estVide() {
+    return this.tableau.length == 0;
+  }
+
+  @Override
+  public int taille() {
+    return this.tableau.length;
+  }
+
+  @Override
+  public boolean contient(E e) {
+    for (int i = 0; i < this.tableau.length; i++) {
+      if (this.tableau[i].equals(e)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
