340433b34a01283956da033f1b3bcf5d791457d9
Iulian Gainar <66164914+juliangainar@users.noreply.github.com>
2023-02-11 15:33:37 +0000

    Added : - Code partiel de la classe ListeChainee
    Commentaires :
    - Reste encore quelques fonctions à rajouter

--- fr/README.md
+++ fr/README.md
@@ -1,2 +1,6 @@
-Question : quel est le type du tableau à utiliser ?
+- Question : quel est le type du tableau à utiliser ?
 Vu que la classe est générique (ListeTableau<E>), le type de tableau serait le type générique E : private E[] tableau;
+
+- Question : Selon vous, quelles sont les différences majeures entre ListeTableau et ListeChainee ?
+  ListeTableau contient un tableau, ce qui nous donne acces immediat aux elements a travers l'indice. Elle possede ainsi une taille fixe (qu'on augmente à chaque fois qu'on l'attend).
+  ListeChainee contient une liste, ce qui nous donne un acces séquentiel, un par un en partant du premier element. De plus, sa taille peut augmenter indéfiniment, on pourra toujours ajouter un element de plus à la liste.
--- fr/rodez3il/a2022/mrmatt/solveur/structures/ListeChainee.java
+++ fr/rodez3il/a2022/mrmatt/solveur/structures/ListeChainee.java
@@ -1,6 +1,8 @@
 package fr.rodez3il.a2022.mrmatt.solveur.structures;
 
 public class ListeChainee<T> implements Liste<T> {
+  // cours utilisé
+  // http://deptinfo.cnam.fr/Enseignement/CycleA/APA/nfa032/docs/cours-listes.pdf
 
   // Classes
 
@@ -11,16 +13,22 @@ public class ListeChainee<T> implements Liste<T> {
     private Maillon suivant;
 
     // constructeurs
-    public Maillon(T donnee, Maillon suivant){
-      this.donnee =  donnee;
+    public Maillon(T donnee, Maillon suivant) {
+      this.donnee = donnee;
       this.suivant = suivant;
     }
-    
+
   }
 
   // Attributs
+  private Maillon premierElement;
+  private int taille;
 
   // Constructeurs
+  public ListeChainee() {
+    this.premierElement = null;
+    this.taille = 0;
+  }
 
   // Accesseurs
 
@@ -34,7 +42,22 @@ public class ListeChainee<T> implements Liste<T> {
    */
   @Override
   public void ajouter(T element) {
-
+    // on cree le nouveau maillon à rajouter à la liste
+    Maillon nouveauMaillon = new Maillon(element, null);
+    // on parcourt la liste et on l'ajoute en dernier
+    // si la liste est vide, on l'ajoute en tant que premier element
+    if (this.estVide()) { // ou this.premierElement == null
+      this.premierElement = nouveauMaillon;
+    } else {
+      // sinon, on cherche le dernier maillon et on l'ajoute à la suite
+      Maillon dernierMaillon = this.premierElement;
+      while (dernierMaillon.suivant != null) {
+        dernierMaillon = dernierMaillon.suivant; // (recursivité, un maillon dans un maillon dans un maillon...)
+      }
+      dernierMaillon.suivant = nouveauMaillon;
+    }
+    // on augmente la taille de la liste
+    this.taille++;
   }
 
   /**
@@ -45,7 +68,7 @@ public class ListeChainee<T> implements Liste<T> {
    */
   @Override
   public boolean estVide() {
-
+    return this.premierElement == null;
   }
 
   /**
@@ -56,7 +79,7 @@ public class ListeChainee<T> implements Liste<T> {
    */
   @Override
   public int taille() {
-
+    return this.taille;
   }
 
   /**
@@ -69,7 +92,32 @@ public class ListeChainee<T> implements Liste<T> {
    */
   @Override
   public T enlever(int i) {
+    // il faut parcourir la liste en recherche du dernier maillon
 
+    // si la liste est vide, ou si i depasse les bords de la liste, on ne peut
+    // rien supprimer
+    if (this.estVide() || i < 0 || i > this.taille) { // ou this.premierElement == null
+      System.err.println("La liste est vide ou le numéro ne correspond dépasse la taille de la liste.");
+    } else {
+      // sinon il faut trouver le maillon à l'index i et le supprimer et retourner
+      // la donnee de l'element supprime
+      Maillon maillonAvant = null;
+      Maillon maillonApres = this.premierElement;
+
+      // on parcourt la liste et on garde le maillon i et i-1
+      for (int j = 0; j < i; j++) {
+        maillonAvant = maillonApres;
+        maillonApres = maillonApres.suivant;
+      }
+
+      // on echange les references des deux maillons = "supprimer"
+      maillonAvant.suivant = maillonApres.suivant;
+
+      // on garde la donnée pour la renvoyer
+      T donneeMaillonSupprime = maillonApres.donnee;
+    }
+
+    this.taille--;
   }
 
   /**
@@ -91,8 +139,8 @@ public class ListeChainee<T> implements Liste<T> {
    * @authors Iulian GAINAR
    * @param e L'élément à comparer.
    * 
+   **/
 
-  
   @Override
   public boolean contient(T e) {
 
