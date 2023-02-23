9cb0c28c5e5f3f6a1e5b4d21aadcae441007b60c
Iulian Gainar <66164914+juliangainar@users.noreply.github.com>
2023-02-23 11:47:21 +0000

    Added : - Importé les sources de Mr Matt
    Modified :
    - Code de la classe Noeud --> A tester

--- fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
@@ -1,7 +1,7 @@
 package fr.rodez3il.a2022.mrmatt.solveur;
 
 import fr.rodez3il.a2022.mrmatt.solveur.structures.*;
-import fr.rodez3il.a2022.mrmatt.sourcespartielles.*;
+import fr.rodez3il.a2022.mrmatt.sources.*;
 
 public class Noeud {
 
@@ -11,7 +11,6 @@ public class Noeud {
   private ListeTableau<Noeud> fils; // represente les quatre fils
   private ListeChainee<Commande> commandes; // succession de commandes necessaires pour atteindre cette configuration
   private boolean visite; // indique si le noeud est visité ou non
-
 
   // Constructeurs
   public Noeud(DictionnaireChaine<String, Noeud> confConnues, Niveau etatNiveau, ListeTableau<Noeud> fils,
@@ -53,30 +52,43 @@ public class Noeud {
 
   /**
    * Methode calculerFils
-   * Calcule
+   * Calcule les prochains mouvements (si possibles)
    * 
    * @params Aucun
    * @returns void
    * @authors Iulian GAINAR
    */
-  public void calculerFils() {
+  public String calculerFils() {
     // on considere que le noeud est visite
     this.visite = true;
     // pour chaque direction (haut, bas, gauche, droite)
-    for (Commande dir : Commande.values()) {
-      if(dir == dir.HAUT){
-        Niveau niveauFils = niveauFils.deplacer(dir.HAUT);
+    for (Commande direction : Commande.values()) {
+      // si le deplacement est possible dans cette direction
+      if (this.etatNiveau.deplacementPossible(direction)) {
+        // on copie l'etat du niveau
+        Niveau niveauFils = this.etatNiveau.copier();
+        // on effectue le deplacement
+        niveauFils.deplacer(direction);
+
+        // on verifie si cette configuration a deja ete visitee
+        String valeurFils = niveauFils.valeurChaine();
+        Noeud noeudFils = this.confConnues.valeur(valeurFils);
+        // si le niveau est gagnant on renvoie la chaine de direction gagnante
+        if (niveauFils.estGagnant()) {
+          return valeurFils;
+        } else if (noeudFils == null) { // si on n'a pas encore visite cette configuration
+          // on cree un nouveau noeud et on l'ajoute a la liste des fils
+          ListeChainee<Commande> nouvellesCommandes = this.commandes;
+          // on ajoute la commande
+          nouvellesCommandes.ajouter(direction);
+          noeudFils = new Noeud(this.confConnues, niveauFils, this.fils, nouvellesCommandes);
+          this.fils.ajouter(noeudFils);
+        } else {
+          // sinon on ajoute ce noeud comme fils
+          this.fils.ajouter(noeudFils);
+        }
       }
-      if(dir == dir.BAS){
-        
-      }
-      if(dir == dir.GAUCHE){
-        
-      }
-      if(dir == dir.DROITE){
-        
-      }
-      
     }
+    return null;
   }
 }
--- fr/rodez3il/a2022/mrmatt/sourcespartielles/Niveau.java
+++ fr/rodez3il/a2022/mrmatt/sourcespartielles/Niveau.java
@@ -2,51 +2,58 @@ package fr.rodez3il.a2022.mrmatt.sourcespartielles;
 
 /**
  * Fourretout temporaire avant le 9/12/22
+ * 
  * @author proussille
  *
  */
 
-public class Niveau{
+public class Niveau {
 
-	///////////////////////////////////////////////////////////////////////
+  // Constructeur
+  public Niveau(Niveau autreNiveau) {
+  }
 
-	// Méthodes supplémentaires pour le solveur.
+  ///////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Indique si le déplacement passé en paramètre est possible.
-	 * 
-	 * @param direction
-	 * @return VRAI ssi le déplacement est possible.
-	 */
-	public boolean deplacementPossible(Commande direction) {
-		return false;
-	}
+  // Méthodes supplémentaires pour le solveur.
 
-	/**
-	 * Réalise le déplacement passé en paramètre sur le niveau.
-	 * @param direction Le déplacement à réaliser.
-	 */
-	public void deplacer(Commande direction) {
-	}
+  /**
+   * Indique si le déplacement passé en paramètre est possible.
+   * 
+   * @param direction
+   * @return VRAI ssi le déplacement est possible.
+   */
+  public boolean deplacementPossible(Commande direction) {
+    return false;
+  }
 
-	/**
-	 * Calcule l'état fixe suivant (saute les états intermédiaires).
-	 */
-	public void calcule() {
+  /**
+   * Réalise le déplacement passé en paramètre sur le niveau.
+   * 
+   * @param direction Le déplacement à réaliser.
+   */
+  public void deplacer(Commande direction) {
+  }
 
-	}
+  /**
+   * Calcule l'état fixe suivant (saute les états intermédiaires).
+   */
+  public void calcule() {
 
-	/**
-	 * Accesseur pour savoir si le joueur a gagné.
-	 */
-	public boolean estGagnant() {
-		return false;
-	}
+  }
 
-	/**
-	 * Renvoie la valeur du niveau sous forme de chaîne (pour déterminer les doublons).
-	 */
-	public String valeurChaine() {
-		return "";
-	}
+  /**
+   * Accesseur pour savoir si le joueur a gagné.
+   */
+  public boolean estGagnant() {
+    return false;
+  }
+
+  /**
+   * Renvoie la valeur du niveau sous forme de chaîne (pour déterminer les
+   * doublons).
+   */
+  public String valeurChaine() {
+    return "";
+  }
 }
